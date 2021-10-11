b0487b7a0b19f1e5a2c6f41bd5e7de7f8cca86e6
claravpacheco <clara.pacheco@ing.austral.edu.ar>
2021-10-11 18:14:58 -0300

    modified file

--- gestiondeusuarios.py
+++ gestiondeusuarios.py
@@ -1,3 +1,14 @@
+
 class GestiónDeUsuarios:
+    Usuarios = []
     def __init__(self,phone,cuil):
+        self.phone = phone
+        self.cuil = cuil
+    def FindUsuariobyPhone(self, usuario):
+        for usuario in Usuarios:
+            pass
+    def FindUsuariobyCuil(self, usuario):
+        for usuario in Usuarios:
+            pass
+
         
